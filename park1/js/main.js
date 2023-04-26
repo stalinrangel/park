0b1fc01d7cc2cda47fd30e52d2f2616332ba4d9f
stalinrangel <e.stalinrangel@gmail.com>
2023-04-26 04:41:39 +0200

    entrega 1

---
+++ park1/js/main.js
@@ -0,0 +1,132 @@
+$(document).ready(function(){
+  
+  window.onload = function () {
+
+    var ln = navigator.language || navigator.userLanguage;
+    /*Validar que no se encuentre en la pagina correspondiente a su idioma*/
+    let pagActual = window.location.pathname;
+    
+    //console.log(ln)
+    if (ln == 'en-EN') {
+      console.log('en')
+      loadLang('en');
+      setSelectedValue("English");
+    } else if (ln == 'es-ES') {
+      console.log('es')
+      loadLang('es');
+      setSelectedValue("Español");
+    } else if (ln == 'it-IT'){
+      console.log('it')
+      loadLang('it');
+      setSelectedValue("Français");
+    }else if (ln == 'fr-FR'){
+      console.log('fr')
+      loadLang('fr');
+      setSelectedValue("Italiano");
+    } else{
+      console.log("Otro idioma");
+    }
+ }
+
+  var elem = document.getElementById( 'test' );
+  
+  elem.onclick = function ( e ) {
+    //console.log(e.target.value)
+      if ( e.target.value == 'English' ) {
+          console.log( 'English' );
+          loadLang('en');
+      }
+      if ( e.target.value == 'Español' ) {
+        console.log( 'Español' );
+        loadLang('es');
+    
+      }
+      if ( e.target.value == 'Français' ) {
+        console.log( 'Français' );
+        loadLang('fr');
+      }
+      if ( e.target.value == 'Italiano' ) {
+        console.log( 'Italiano' );
+        loadLang('it');
+      }
+  };
+
+  var setSelectedValue = function(val) {
+    var selectElement = document.getElementById("test");
+    selectElement.value = val;
+    console.log(val)
+  }
+
+  var selector = '#translate';
+  $(selector).on('click', function(e){
+
+    e.preventDefault();
+    startLang( $(this) );
+  });
+  var startLang = function(el){
+    
+    var el = $(el);
+    var text = el.attr('data-text');
+    var file = el.attr('data-file');
+    console.log(file)
+    file = file.split(',');
+    text = text.split(',');
+    var index = el.attr('data-index');
+    if(index >= file.length){
+      index = 0;
+    }
+    changeName(el, text[index]);
+    changeIndex(el, index);
+    loadLang(file[index]);
+    $('html').attr('lang', file[index]);
+  };
+
+  var changeName = function(el, name){
+    console.log(el,name)
+    $(el).html( name );
+  };
+
+  var changeIndex = function(el, index){
+    console.log(el,index)
+    $(el).attr('data-index', ++index);
+  };
+
+  var loadLang = function(lang){
+    console.log(lang)
+    var processLang = function(data){
+      var arr = data.split('\n');
+      for(var i in arr){
+        if( lineValid(arr[i]) ){
+          var obj = arr[i].split('=>');
+          assignText(obj[0], obj[1]);
+        }
+      }
+    };
+    var assignText = function(key, value){
+      $('[data-lang="'+key+'"]').each(function(){
+        var attr = $(this).attr('data-destine');
+        if(typeof attr !== 'undefined'){
+          $(this).attr(attr, value);
+        }else{
+          $(this).html(value);
+        }
+      });
+    };
+    var lineValid = function(line){
+      return (line.trim().length > 0);
+    };
+    $('.loading-lang').addClass('show');
+    $.ajax({
+      url: 'lang/'+lang+'.txt',
+      error:function(){
+        alert('No se cargó traducción');
+      },
+      success: function(data){
+        $('.loading-lang').removeClass('show');
+        processLang(data);
+      }
+    });
+  };  
+
+  
+});
